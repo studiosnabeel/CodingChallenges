a6e3013ffebb2a16787adba2242f0bab9bede14a
Nabeel Akbar <studiosnabeel@gmail.com>
2023-06-15 13:48:14 +0500

    swap exercise solved

---
+++ swap-variables/swapVariables.js
@@ -0,0 +1,15 @@
+// How to swap the values of variables?
+let a = 'red';
+let b = 'blue';
+
+//new variable and make it's value equal to a
+let c = a;
+
+//Make variable a value equal to b so that it turns blue
+a = b;
+
+//Final step: make variable b equal to value of variable c
+b = c;
+
+console.log(a);
+console.log(b);
