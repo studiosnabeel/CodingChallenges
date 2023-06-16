939594d49f457773e90e7bf35f6ea43b813f15eb
Nabeel Akbar <studiosnabeel@gmail.com>
2023-06-16 11:30:48 +0500

    preferred way

--- portrait-landscape/portraitLandscape.js
+++ portrait-landscape/portraitLandscape.js
@@ -1,8 +1,10 @@
 function isLandscape(width, height) {
   // if (width > height) return true;
   // else return false;
+
   // return width > height ? true:false;
 
+  //the preferred way of writing the code
   return width > height;
 }
 
